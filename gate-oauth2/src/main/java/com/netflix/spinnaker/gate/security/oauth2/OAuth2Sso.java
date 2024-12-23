1bf91bacc13c86c4309593e99d70073791e1d4a9
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2024-12-23 08:15:10 +0530

    refactor(OAuth2): Implemented OAuth2Sso as per new dsl introduced in Spring Security 5.2.
    Old way of OAuth2 configuration(using @EnableOAuth2Sso) was deprecated in Spring Security 5.2 and later removed in Spring Security 6. Please see reference links down below for more details.
    
    ```
    OAuth2Spec > should redirect on oauth2 authentication FAILED
        Condition not satisfied:
    
        result.response.getStatus() == 302
        |      |        |           |
        |      |        200         false
        |      <org.springframework.mock.web.MockHttpServletResponse@4e5e430f outputStreamAccessAllowed=true writerAccessAllowed=true defaultCharacterEncoding=ISO-8859-1 characterEncoding=UTF-8 characterEncodingSet=true content=[] outputStream=org.springframework.mock.web.MockHttpServletResponse$ResponseServletOutputStream@20c406ea writer=null contentLength=2 contentType=application/json;charset=UTF-8 bufferSize=4096 committed=true locale=en_US cookies=[] headers=[Access-Control-Allow-Credentials:[true], Access-Control-Allow-Origin:[*], Access-Control-Allow-Methods:[POST, GET, OPTIONS, DELETE, PUT, PATCH], Access-Control-Max-Age:[3600], Access-Control-Allow-Headers:[x-requested-with, content-type, authorization, X-RateLimit-App, X-Spinnaker-Priority], Access-Control-Expose-Headers:[X-AUTH-REDIRECT-URL], X-SPINNAKER-REQUEST-ID:[c567e7b8-a152-42cc-8c28-206c4408f476], Content-Type:[application/json;charset=UTF-8], Content-Length:[2]] status=200 errorMessage=null forwardedUrl=null includedUrls=[]>
        <org.springframework.test.web.servlet.DefaultMvcResult@3ac383ad mockRequest=org.springframework.mock.web.MockHttpServletRequest@11e328ac mockResponse=org.springframework.mock.web.MockHttpServletResponse@4e5e430f handler=com.netflix.spinnaker.gate.controllers.CredentialsController#getAccounts(User, boolean) interceptors=[com.netflix.spinnaker.kork.web.interceptors.MetricsInterceptor@57c9b4b3, com.netflix.spinnaker.gate.interceptors.ResponseHeaderInterceptor@71b19b0c, com.netflix.spinnaker.gate.interceptors.RequestContextInterceptor@799f7bde, org.springframework.web.servlet.handler.ConversionServiceExposingInterceptor@45e519d9, org.springframework.web.servlet.resource.ResourceUrlProviderExposingInterceptor@5f1341fa] modelAndView=null resolvedException=null asyncResult=java.lang.Object@2287d8ef asyncDispatchLatch=null>
            at com.netflix.spinnaker.gate.security.oauth2.OAuth2Spec.should redirect on oauth2 authentication(OAuth2Spec.groovy:54)
    ```
    Reference Links:
    
    https://docs.spring.io/spring-security/reference/5.8/migration/servlet/oauth2.html#_address_oauth2_client_deprecations
    https://github.com/spring-projects/spring-security/wiki/OAuth-2.0-Migration-Guide

---
+++ gate-oauth2/src/main/java/com/netflix/spinnaker/gate/security/oauth2/OAuth2Sso.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.gate.security.oauth2;
+
+import com.netflix.spinnaker.gate.config.AuthConfig;
+import com.netflix.spinnaker.gate.security.SpinnakerAuthConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+@EnableWebSecurity
+@SpinnakerAuthConfig
+@Conditional(OAuth2ConfigCondition.class)
+public class OAuth2Sso {
+
+  @Autowired
+  AuthConfig authConfig;
+
+  @Autowired
+  DefaultCookieSerializer defaultCookieSerializer;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    defaultCookieSerializer.setSameSite(null);
+    authConfig.configure(httpSecurity);
+    return httpSecurity.authorizeHttpRequests
+        (auth -> auth.anyRequest().authenticated())
+      .oauth2Login(Customizer.withDefaults())
+      .build();
+  }
+}
